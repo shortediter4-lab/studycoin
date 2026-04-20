420f187dd89b978577b8d401ba3a6f15e2e323f0
StudyCoin STC <shortediter4@gmail.com>
2026-04-20 08:43:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAdSDs1QtO2lai8DzA4JxvKg7Nf1lDk5EI",
-  authDomain: "studycoin-stc.firebaseapp.com",
-  databaseURL: "https://studycoin-stc-default-rtdb.firebaseio.com",
-  projectId: "studycoin-stc",
-  storageBucket: "studycoin-stc.firebasestorage.app",
-  messagingSenderId: "840351904766",
-  appId: "1:840351904766:web:7ec85a6e8d2cdfa4c246af"
-};
+
