6a19444d4971375ad4d5245964d0ef74b376bfd0
StudyCoin STC <shortediter4@gmail.com>
2026-04-21 13:57:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("YOUR_MONGO_URL");
+
+const User = mongoose.model("User", {
+  username: String,
+  coins: { type: Number, default: 0 }
+});
+
+// LOGIN / CREATE USER
+app.post("/login", async (req, res) => {
+  let user = await User.findOne({ username: req.body.username });
+
+  if (!user) {
+    user = await User.create({ username: req.body.username });
+  }
+
+  res.json(user);
+});
+
+// ADD COINS
+app.post("/add", async (req, res) => {
+  const user = await User.findById(req.body.id);
+  user.coins += 10;
+  await user.save();
+  res.json(user);
+});
+
+// GET USER
+app.get("/user/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
+});
+
+app.listen(5000, () => console.log("Server running"));
